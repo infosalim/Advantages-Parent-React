74bd6a90407fbd5533a807632556227fd3054660
infosalim <salimh5757@gmail.com>
2021-05-26 01:23:40 +0600

    css fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { Switch, Route } from 'react-router-dom';
+// Styles
 import './assets/styles/App.css';
+// Layout
+import Layout from './Layout/Layout';
+// Pages
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>App</div>
+    <Layout>
+      <Switch>
+         <Route path='/'>
+          <Home />
+        </Route>
+        {/*<Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route> */}
+      </Switch>
+    </Layout>
   );
 }
 
